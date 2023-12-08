f83d65ab7dfe90be58a56c23129626aa48eead2d
joeeid <joe.eid2000@gmail.com>
2023-12-08 10:14:11 +0200

    Initial Commit

---
+++ bot.js
@@ -0,0 +1,52 @@
+import TelegramBot from "node-telegram-bot-api";
+import qr from "qr-image";
+import fs from "fs";
+
+const token = "6969319096:AAEtkwsqQjS0kKW3eup--wcNoFlVvDoK21E";
+const bot = new TelegramBot(token, { polling: true });
+var started;
+
+function isValidUrl(string) {
+	try {
+		new URL(string);
+		return true;
+	} catch (err) {
+		return false;
+	}
+}
+
+bot.on("message", (msg) => {
+	const chatId = msg.chat.id;
+	const messageText = msg.text;
+
+	if (messageText === "/start") {
+		if (!started) {
+			started = true;
+			bot.sendMessage(
+				chatId,
+				"Welcome to the Qr Generator bot! Send me a link and you shall receive your QR Code image."
+			);
+		}
+	} else if (started) {
+		var url = messageText;
+		if (!isValidUrl(messageText)) {
+			url = `https://${messageText}`;
+		}
+		if (isValidUrl(url)) {
+			const photoPath = "qrcode.png";
+			try {
+				fs.unlinkSync(photoPath);
+			} catch (err) {
+				//File already deleted.
+			}
+			var qrCode = qr.image(url, { type: "png" });
+			var writeStream = fs.createWriteStream(photoPath);
+			qrCode.pipe(writeStream);
+			writeStream.on("finish", () => {
+				bot.sendPhoto(chatId, photoPath, {
+					caption: `QR Code of ${url}`,
+				});
+			});
+		}
+	}
+});
